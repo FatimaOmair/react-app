db064a5491598e0f262b3b81adbb1eab3e334221
FatimaOmair <fatimaemer2002@gmail.com>
2023-10-29 17:19:56 +0300

    vit app

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './components/navbar/Navbar.jsx'
+import Home from './components/Home/Home.jsx'
+import Products from './components/products/Products.jsx'
+import Footer from './components/footer/Footer.jsx'
+export default function App() {
+  return (
+   <>
+   <Navbar/>
+   <Home/>
+   <Products/>
+   <Footer/>
+
+   
+   </>
+  )
+}
